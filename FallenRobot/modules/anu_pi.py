709523a559b29ffe28d196d57a23f8609d2a84aa
Ananya <90745373+anunicn47@users.noreply.github.com>
2023-03-25 12:39:10 +0530

    see.now

---
+++ FallenRobot/modules/anu_pi.py
@@ -0,0 +1,22 @@
+import requests
+
+from pyrogram import Client, filters
+
+
+
+url = "https://api.writesonic.com/v2/business/content/chatsonic?engine=premium"
+@Client.on_message(filters.command('/cmd'))
+async def anu_pii(bot, m):
+    text = m.text[len('/cmd ') :]
+    payload = {
+    "enable_google_results": "true",
+    "enable_memory": False,
+    "input_text": f"{text}"
+    }
+    headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "X-API-KEY": "7384fc5d-3a89-44b6-b8c0-3f98c689d87f"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    await m.reply(response.text)

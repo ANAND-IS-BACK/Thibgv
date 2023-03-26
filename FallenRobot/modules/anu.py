1dade39e2a484e3ba55f536adcf6d2f7f5acd27b
LinuxGuy312 <101087324+LinuxGuy312@users.noreply.github.com>
2023-03-26 19:52:53 +0530

    Update

--- FallenRobot/modules/anu.py
+++ FallenRobot/modules/anu.py
@@ -1,17 +1,17 @@
 import requests
-from FallenRobot import pbot as Client
-from pyrogram import Client, filters
+from FallenRobot import pbot
+from pyrogram import filters
 
 
 
 url = "https://api.writesonic.com/v2/business/content/chatsonic?engine=premium"
-@Client.on_message(filters.command('/ask'))
+@pbot.on_message(filters.command('/ask'))
 async def anu_pii(bot, m):
-    text = m.text[len('/ask ') :]
+    text = m.text.split(' ', 1)[1]
     payload = {
     "enable_google_results": "true",
     "enable_memory": False,
-    "input_text": f"{text}"
+    "input_text": text
     }
     headers = {
     "accept": "application/json",
@@ -19,4 +19,4 @@ async def anu_pii(bot, m):
     "X-API-KEY": "7384fc5d-3a89-44b6-b8c0-3f98c689d87f"
     }
     response = requests.post(url, json=payload, headers=headers)
-    await m.reply(response.text)
+    await m.reply(response.text, quote=True)
